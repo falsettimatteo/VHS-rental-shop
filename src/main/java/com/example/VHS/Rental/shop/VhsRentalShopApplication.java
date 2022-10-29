6af8acaeabcbfa8284dbbc2a63a2e535d6f27e88
falsettimatteo <matteofalsetti25gmail.com>
2022-10-29 09:48:43 +0200

    first commit

---
+++ src/main/java/com/example/VHS/Rental/shop/VhsRentalShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.VHS.Rental.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsRentalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhsRentalShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VHS/Rental/shop/VhsRentalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VHS.Rental.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsRentalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
